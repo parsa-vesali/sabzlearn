08598d124ee97eac816fbf79e8c634d5253e4bef
Parsa Vesali <parsavesali84@gmail.com>
2024-05-08 15:03:37 +0330

    navbar updated

--- src/Components/AccountBox.jsx
+++ src/Components/AccountBox.jsx
@@ -1,18 +1,79 @@
 import React, { useState } from 'react'
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import FolderCopyOutlinedIcon from '@mui/icons-material/FolderCopyOutlined';
+import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 export default function AccountBox() {
     const [isShowBox, setIsShowBox] = useState(false)
-    return (
-        <div className='relative'>
-            <button className='dark:bg-dark-500 bg-gray-200 text-slate-500   dark:text-gray-100 cursor-pointer p-3 rounded-full'>
-                <PersonOutlinedIcon style={{ fontSize: '1.6rem' }} />
-            </button>
 
-            <div className='absolute shadow-lg bg-dark-700 space-y-5 top-20 left-0 w-64 p-6 text-gray-100 rounded-lg border-t-2 border-t-green-500 transition-all delay-75 invisible opacity-0 group-hover:top-16 group-hover:opacity-100 group-hover:visible child:transition-colors child-hover:text-green-500 flex items-start flex-col child:block child:text-base line-clamp-1'>
-                <h2>پارسا وصالی</h2>
+    const clickHanlder = () => {
+        setIsShowBox(!isShowBox)
+    }
+
+    return (
+        <>
+            <div className='relative z-40'>
+                <button onClick={clickHanlder} className='dark:bg-dark-500 bg-gray-200 text-slate-500   dark:text-gray-100 cursor-pointer p-3 rounded-full'>
+                    {
+                        isShowBox ? (
+                            <CloseIcon style={{ fontSize: '1.6rem' }} />
+                        ) : (
+                            <PersonOutlinedIcon style={{ fontSize: '1.6rem' }} />
+                        )
+                    }
+                </button>
+
+                <div className={`absolute shadow-lg bg-white  dark:bg-dark-700 space-y-2 top-16 left-0  w-72 py-3 px-4 text-zinc-700 dark:text-gray-100 rounded-xl border-t-4 border-t-green-500 transition-all delay-75 ${isShowBox ? 'visible opacity-100' : 'invisible opacity-0'} child:transition-colors  flex items-start flex-col `}>
+
+                    <div className="w-full flex items-center justify-staer gap-x-4 border-b border-b-neutral-200 dark:border-b-white/5 pb-4 ">
+                        <div className="">
+                            <img className='rounded-full w-16 h-16' src="./images/profile.png" alt="" />
+                        </div>
+                        <div className="space-y-2">
+                            <p className='text-lg font-Dana-Bold'>پارسا وصالی</p>
+                            <p className='text-green-500'>موجودی 0</p>
+                        </div>
+                    </div>
+
+                    <div className='flex w-full flex-col gap-y-1 child-hover:bg-green-600 text-lg child:transition-all border-b border-b-neutral-200 dark:border-b-white/5 pb-4'>
+                        <a href="#" className='flex items-center gap-x-2 py-3 rounded-lg px-2'>
+                            <HomeOutlinedIcon />
+                            پیشخوان
+                        </a>
+                        <a href="#" className='flex items-center gap-x-2 py-3 rounded-lg px-2'>
+                            <FolderCopyOutlinedIcon />
+                            دوره های من
+                        </a>
+                        <a href="#" className='flex items-center gap-x-2 py-3 rounded-lg px-2'>
+                            <PersonOutlineOutlinedIcon />
+                            جزئیات حساب
+                        </a>
+                        <a href="#" className='flex items-center gap-x-2 py-3 rounded-lg px-2'>
+                            <ForumOutlinedIcon />
+                            تیکت ها
+                        </a>
+                    </div>
+
+                    <div className="flex w-full flex-col gap-y-1 child-hover:bg-red-600 text-lg child:transition-all">
+                        <a href="#" className='flex items-center gap-x-2 py-3 rounded-lg px-2 '>
+                            <PowerSettingsNewOutlinedIcon />
+                            خروج
+                        </a>
+                    </div>
+
+                </div>
+
             </div>
 
-        </div>
+            {
+                isShowBox && (
+                    <div className='fixed w-full h-full top-0 left-0 bg-black/40 z-10 md:backdrop-blur transition-all show'></div>
+                )
+            }
+        </>
     )
 }
