3319dfdfddce4a44b5df3cb19a058c0ab54b9a3f
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-05-12 20:16:22 +0330

    the project updated

---
+++ src/Components/CourseInfoBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function CourseInfoBox({ title, subtitle, children }) {
+    return (
+        <div className="flex flex-col md:flex-row text-center md:text-right  items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white dark:bg-dark-700  pt-4 pb-3.5 sm:py-3 px-4 rounded-xl">
+            <span className='text-green-500'>
+                {
+                    children
+                }
+            </span>
+            <span className=' space-y-1'>
+                <h2 className=' text-lg'>{title}</h2>
+                <p className=' block dark:text-gray-400  text-sm font-Dana-Bold'>{subtitle}</p>
+            </span>
+        </div>
+    )
+}
--- src/Pages/MainCourse.jsx
+++ src/Pages/MainCourse.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Notifications from '../Components/Notifications'
 import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
 import Breadcrumb from '../Components/BreadCrumb';
 import Button from '../Components/Common/Button';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+import GroupIcon from '@mui/icons-material/Group';
+import StarIcon from '@mui/icons-material/Star';
+import DescriptionIcon from '@mui/icons-material/Description';
+import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ForumIcon from '@mui/icons-material/Forum';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import CourseInfoBox from '../Components/CourseInfoBox';
 
 export default function MainCourse() {
     return (
         <>
-            <div className='container py-8 bg-gray-100 dark:bg-dark-900 text-zinc-600 dark:text-gray-100'>
+            <div className='container py-8 bg-gray-100 dark:bg-dark-900 text-zinc-900 dark:text-gray-100'>
                 <Notifications />
             </div>
 
@@ -17,7 +33,7 @@ export default function MainCourse() {
             <NavBar />
 
 
-            <div className="container dark:bg-dark-900 text-zinc-600 dark:text-gray-100">
+            <div className="container dark:bg-dark-900 text-zinc-900 dark:text-gray-100">
                 <Breadcrumb route1='دوره ها' route2='بک اند' route3='آموزش پروژه محور فریمورک Fastify' />
 
                 {/* title and demo  */}
@@ -26,9 +42,8 @@ export default function MainCourse() {
                     <div className="flex flex-col justify-between h-full ">
                         <div className="">
                             <h2 className='text-[1.625rem] font-Dana-Bold mb-4'>آموزش پروژه محور NestJS از صفر!</h2>
-                            <p className=' text-lg line-clamp-4'>NestJS یه فریم‌ورک توسعه سمت سرور وب با TypeScript برای ساخت برنامه‌های مبتنی بر Node.js هست. نست بر پایه الگوی معماری ماژولار پیاده سازی شده و میتونیم باهاش برنامه‌هایی با خوانایی بسیار بالا بسازیمz</p>
+                            <p className=' text-lg line-clamp-4'>NestJS یه فریم‌ورک توسعه سمت سرور وب با TypeScript برای ساخت برنامه‌های مبتنی بر Node.js هست. نست بر پایه الگوی معماری ماژولار پیاده سازی شده و میتونیم باهاش برنامه‌هایی با خوانایی بسیار بالا بسازیم</p>
                         </div>
-
 
                         <div className="flex flex-col mt-4 lg:mt-0 lg:flex-row justify-between items-center">
                             <Button title='ثبت نام در دوره' icon={<SchoolOutlinedIcon />} />
@@ -39,7 +54,54 @@ export default function MainCourse() {
 
                     <video src="" controls poster='./images/courses/git.webp' className='h-[345px] rounded-xl'></video>
                 </div>
+
+
+
+
+
+
+                {/* Course Data */}
+                <div className="grid grid-cols-12 gap-6 sm:gap-7 mt-7 lg:mt-20">
+
+
+                    {/* Data Right */}
+                    <div className="col-span-12 lg:col-span-8">
+
+                        {/* COURSE DATA BOX */}
+                        <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+
+                            <CourseInfoBox title='وضعیت دوره' subtitle='تکمیل شده' >
+                                <InfoOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+                            <CourseInfoBox title='مدت زمان دوره' subtitle='23 ساعت' >
+                                <AccessTimeOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+                            <CourseInfoBox title='آخرین بروزرسانی' subtitle='1403/02/01' >
+                                <CalendarMonthOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+                            <CourseInfoBox title='روش پشتیبانی' subtitle='آنلاین' >
+                                <PeopleOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+                            <CourseInfoBox title='پیش نیاز' subtitle='HTML & CSS' >
+                                <BusinessCenterOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+                            <CourseInfoBox title='نوع مشاهده' subtitle='آنلاین' >
+                                <VideocamOutlinedIcon style={{ fontSize: '2.7rem' }} />
+                            </CourseInfoBox>
+
+                        </div>
+                    </div>
+
+
+
+                    {/* Data Left */}
+                    <div className=""></div>
+                </div>
+
             </div>
+
+
+            <Footer />
         </>
     )
 }
