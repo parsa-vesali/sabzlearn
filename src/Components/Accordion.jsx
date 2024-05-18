05e3a12fb52da5a8af02220012bbcb665ceb0610
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-05-18 19:23:43 +0330

    the project updated

---
+++ src/Components/Accordion.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+
+
+export default function Accordion() {
+
+    const [isShowAccordion, setIsShowAccordion] = useState(false)
+
+    const showAccordion = () => {
+        setIsShowAccordion(!isShowAccordion)
+    }
+
+
+    return (
+        <div className='relative shadow-lg rounded-lg cursor-pointer overflow-hidden transition-all ' >
+            {/* Accordion title */}
+            <div onClick={showAccordion} className={`flex items-center justify-between  transition-colors ${isShowAccordion ? 'bg-green-500' : 'dark:bg-dark-500 '} px-5 py-4 `} >
+                <h2>معرفی دوره</h2>
+                <div className="flex items-center gap-x-2">
+                    <p>1جلسه</p>
+                    <p>13 دقیقه</p>
+                    <span>
+                        <KeyboardArrowDownIcon style={{ fontSize: '2rem' }} />
+                    </span>
+                </div>
+            </div>
+
+            {/* Accordion body */}
+            <div className={`${isShowAccordion ? 'flex' : 'hidden'} flex-col  dark:bg-dark-500 `}>
+                <div className="flex items-center justify-between px-5 py-4 border-b border-gray-100/10">
+                    <div className="flex items-center gap-x-2">
+                        <span className='w-8 h-8 bg-dark-300 flex items-center justify-center rounded-md'>1</span>
+                        <h1>معرفی فصل - نحوه توسعه پروژه</h1>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                        <p>12:12</p>
+                        <span>
+                            <PlayCircleFilledWhiteOutlinedIcon />
+                        </span>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between px-5 py-4">
+                    <div className="flex items-center gap-x-2">
+                        <span className='w-8 h-8 bg-dark-300 flex items-center justify-center rounded-md'>2</span>
+                        <h1>چه فیچر های جدیدی رو تو پروژه دیوار یاد می‌گیریم</h1>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                        <p>12:12</p>
+                        <span>
+                            <PlayCircleFilledWhiteOutlinedIcon />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Pages/MainCourse.jsx
+++ src/Pages/MainCourse.jsx
@@ -21,8 +21,8 @@ import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ForumIcon from '@mui/icons-material/Forum';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import CourseInfoBox from '../Components/CourseInfoBox';
+import Accordion from '../Components/Accordion';
 
-import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export default function MainCourse() {
     return (
@@ -58,10 +58,6 @@ export default function MainCourse() {
                 </div>
 
 
-
-
-
-
                 {/* Course Data */}
                 <div className="grid grid-cols-12 gap-6 sm:gap-7 mt-7 lg:mt-20">
 
@@ -92,12 +88,68 @@ export default function MainCourse() {
                             </CourseInfoBox>
 
                         </div>
+                        {/* Course description */}
+                        <div className="bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5 mt-8">
+                            <div className="flex items-center gap-x-2 mb-5 sm:mb-6 relative">
+                                <h2 className=' font-Dana-Bold text-2xl flex items-center gap-x-2'>
+                                    <DescriptionIcon className=' text-amber-400' style={{ fontSize: '2rem' }} />
+                                    توضیحات
+                                </h2>
+                                <span className="absolute -right-6 sm:-right-[26px] block w-1.5 h-[34px] md:h-9.5 bg-amber-400 rounded-r-sm "></span>
+                            </div>
+
+                            <div className="mt-10 overflow-hidden rounded-xl w-full  ">
+                                <img src="./images/courses/git.webp" className='w-full ' alt="" />
+                            </div>
+
+
+
+                            <div className='my-10 flex flex-col gap-y-5 child:leading-8 dark:text-zinc-300'>
+                                <p>
+                                    میخواید باهاتون رو راست باشم؟ همین بازیگرای هالیوود. چند درصدشون سیاهی لشکرن و چند درصدشون سوپر استار؟ یا اصلا جای دوری نریم. چند نفر از اطرافیان تون رو میشناسید که کاسب هستن و از بین اون کاسب ها چند نفر واقعا اوضاع خفنی داره؟ طبعا همه میدونیم که تعداد این افراد بسیار بسیار کمه و در دنیای برنامه نویسی. در بخش بازار کار هم اوضاع مستثنی از این نیست. اگر نخواهیم نگاه سوسیالیستی به مسائل داشته باشیم. اینگونه که همه مون بهتره فقیر باشیم و کسی لازم نکرده به جایگاه های بالاتر برسه. باید قبول کنیم که از بین هزاران برنامه نویسی که بصورت کارمندی کار میکنن. تعداد کمی شون دستمزد های بالایی میگیرن. تعداد کمی شون در رده های بالاتر مشغول فعالیتن
+                                </p>
+
+                                <p>
+                                    برای مثال: شما ممکنه با برنامه نویس ایرانیی ملاقات کنید که در عین خوشحالی بگه که من ساعتی تقریبا ۱۷۰ هزار تومان(نسبت به دلار ۵۰ هزار تومانی) دستمزد میگیرم. اما آیا میدونستید که تعداد اندکی از این برنامه نویس ها هستن که دو یا سه یا حتی پنج برابر بیشتر از مبلغ ذکر شده دستمزدشونه؟ شاید در تصوراتتون برای چنین افراد. آدم هایی بیاد که قطعا بالای ۳۰ ساله برنامه نویس هستن. اما نه جانم! مسئله این نیست که شما سابقه بسیار بسیار خفنی داشته باشی. مسئله اینه که چقدر شناخت از کارفرما جماعت داری!</p>
+                            </div>
+
+                            <div className="flex items-center  justify-center w-full">
+                                <Button title='مشاهده مطالب بیشتر'>
+                                    <KeyboardArrowDownOutlinedIcon />
+                                </Button>
+                            </div>
+
+                        </div>
+                        {/* Course headLines */}
+                        <div className="bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5 mt-8">
+                            <div className="flex items-center gap-x-2 mb-5 sm:mb-6 relative">
+                                <h2 className=' font-Dana-Bold text-2xl flex items-center gap-x-2'>
+                                    <DescriptionIcon className=' text-sky-500' style={{ fontSize: '2rem' }} />
+                                    سرفصل ها
+                                </h2>
+                                <span className="absolute -right-6 sm:-right-[26px] block w-1.5 h-[34px] md:h-9.5 bg-sky-500 rounded-r-sm "></span>
+                            </div>
+                            <Accordion />
+                        </div>
+
+                        {/* Course comments */}
+                        <div className="bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5 mt-8">
+                            <div className="flex items-center gap-x-2 mb-5 sm:mb-6 relative">
+                                <h2 className=' font-Dana-Bold text-2xl flex items-center gap-x-2'>
+                                    <DescriptionIcon className=' text-red-500' style={{ fontSize: '2rem' }} />
+                                    نظرات
+                                </h2>
+                                <span className="absolute -right-6 sm:-right-[26px] block w-1.5 h-[34px] md:h-9.5 bg-red-500 rounded-r-sm "></span>
+                            </div>
+
+                        </div>
                     </div>
 
 
 
                     {/* Data Left */}
                     <div className="col-span-12 lg:col-span-4 space-y-8">
+                        {/* STUDENT AND PROGRES BAR */}
                         <div className=" bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5">
                             <div className="flex gap-x-4">
                                 <div className="flex flex-col sm:flex-row items-center text-center md:text-right gap-y-1 gap-x-3 flex-grow pt-3.5 pb-3 sm:px-3.5 sm:py-2.5 bg-gray-100 dark:bg-dark-500 rounded-xl">
@@ -128,12 +180,25 @@ export default function MainCourse() {
                                 <span className=' absolute top-0 bottom-0 left-0 z-20 w-[54%] bg-green-500 rounded-xl'></span>
                             </div>
                         </div>
+                        {/* COURSE TEACHER  */}
                         <div className="bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5 flex flex-col items-center gap-y-4 justify-center">
                             <img className=' w-22 h-22 rounded-full border-2 border-white' src="./images/profile.png" alt="teacher" />
                             <p className=' text-lg font-Dana-Bold'>
                                 رضا دولتی | مدرس دوره
                             </p>
                             <Button title='مشاهده پروفایل من' />
+                        </div>
+                        {/* LINK PROJECT */}
+                        <div className="bg-white dark:bg-dark-700 rounded-xl p-4.5 sm:p-5 flex flex-col items-center gap-y-4 justify-center">
+                            <p className='text-lg font-Dana-Bold'>
+                                لینک کوتاه آموزش
+                            </p>
+                            <div className="flex items-center justify-between w-full cursor-pointer p-5 bg-sky-500/10  text-sky-500 rounded-lg text-xl border border-dashed  border-sky-500">
+                                <span>
+                                    <FileCopyOutlinedIcon />
+                                </span>
+                                <p>sabzlearn.ir/?</p>
+                            </div>
                         </div>
                     </div>
                 </div>
